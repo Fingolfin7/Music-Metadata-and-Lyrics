fe49253c292339225f0c15f7286d5ff516b0d0e9
Henry <63872314+Fingolfin7@users.noreply.github.com>
2021-08-07 02:42:26 +0200

    Update check_internet.py

--- Source/check_internet.py
+++ Source/check_internet.py
@@ -1,6 +1,7 @@
 import socket
 
 
+# check internet connection by trying to ping the google DNS
 def check_internet(host="8.8.8.8", port=53, timeout=3):
     """
     Host: 8.8.8.8 (google-public-dns-a.google.com)
